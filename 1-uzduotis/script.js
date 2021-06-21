22a6682537cfce5edbee7e2acce4381cad571bf4
Vadim Šaranov <vadim.sharanov@gmail.com>
2021-06-21 18:51:11 +0300

    pirma uzduotis done

---
+++ 1-uzduotis/script.js
@@ -0,0 +1,82 @@
+"use strict";
+
+document.getElementById("mygtukas").addEventListener("click", function(){
+    let skaiciusIsIvesties1 = parseFloat(document.getElementById("ivestis1").value);
+    let skaiciusIsIvesties2 = parseFloat(document.getElementById("ivestis2").value);
+
+
+
+const skaiciuotuvas =  {
+
+sk1:0,
+sk2:0,
+
+paleidimas: function (a, b) {
+    this.sk1 = a
+    this.sk2 = b
+    
+
+},
+sudetis: function () {
+    let suma = this.sk1 + this.sk2;
+    return suma;
+},
+
+atimtis: function () {
+    let minus = this.sk1-this.sk2;
+    return minus
+},
+daugyba: function() {
+    let dauginti = this.sk1 * this.sk2;
+    return dauginti;
+},
+
+dalyba: function () {
+    let dalinti = this.sk1 / this.sk2;
+    return dalinti;
+},
+
+saknis: function () {
+    let traukiamSakni1 = Math.sqrt(this.sk1)
+    let traukiamSakni2 = Math.sqrt(this.sk2)
+    return traukiamSakni1, traukiamSakni2
+},
+
+liekana: function () {
+    let ieskomLiekana = this.sk1 % this.sk2
+    return ieskomLiekana
+},
+
+skMasyvas: function () {
+    let skMasyvas = []
+    return skMasyvas;
+},
+
+rezultas: function () {
+    
+    let perdavimas;
+    this.skMasyvas = perdavimas;
+     perdavimas = [this.sudetis(),this.atimtis(),this.daugyba(),this.dalyba (),this.saknis (),this.liekana()];
+     return perdavimas
+},
+vidurkis: function (){
+    let masyvas, vidurkis, indekas, skaicius, suma;
+    indekas = 0; skaicius = 0; suma = 0;
+    masyvas = this.rezultas();
+    while (indekas < masyvas.length) {
+        skaicius = masyvas[indekas];
+        suma += skaicius;
+        indekas = indekas + 1 ;
+    }
+    masyvas[masyvas.length] = suma;
+     return console.log("suma =  " + suma, this.rezultas());
+}
+
+}
+skaiciuotuvas.paleidimas(skaiciusIsIvesties1, skaiciusIsIvesties2)
+
+let atsakymas = skaiciuotuvas.vidurkis()
+
+
+})
+
